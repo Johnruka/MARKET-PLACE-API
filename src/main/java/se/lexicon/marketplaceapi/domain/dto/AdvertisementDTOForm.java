c46ae079af21447de0c11c82780380e45a7a790a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-17 13:05:26 +0200

    Added getters, setters and constructors

--- src/main/java/se/lexicon/marketplaceapi/domain/dto/AdvertisementDTOForm.java
+++ src/main/java/se/lexicon/marketplaceapi/domain/dto/AdvertisementDTOForm.java
@@ -1,4 +1,29 @@
 package se.lexicon.marketplaceapi.domain.dto;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
 public class AdvertisementDTOForm {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private List<String> attachments;
+
+    private LocalDate createDateTime;
+
+    private LocalDate expiryDateTime;
+
+    private PersonDTOForm person;
 }
