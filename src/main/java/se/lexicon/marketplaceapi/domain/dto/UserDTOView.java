ed4363ed0d3b896f206da951564fb8e83eedc86b
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-17 13:16:51 +0200

    added getters, setters and constructors

--- src/main/java/se/lexicon/marketplaceapi/domain/dto/UserDTOView.java
+++ src/main/java/se/lexicon/marketplaceapi/domain/dto/UserDTOView.java
@@ -1,4 +1,18 @@
 package se.lexicon.marketplaceapi.domain.dto;
 
+
+import lombok.*;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Builder
 public class UserDTOView {
+
+    private String email;
+    private Set<RoleDTOView> roles;
 }
