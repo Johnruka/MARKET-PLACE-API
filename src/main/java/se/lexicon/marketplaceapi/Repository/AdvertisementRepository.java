d25e4a7b4cfe31b195c7ccec9548f31471fa9b95
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-17 17:14:53 +0200

    added a bean notation

--- src/main/java/se/lexicon/marketplaceapi/Repository/AdvertisementRepository.java
+++ src/main/java/se/lexicon/marketplaceapi/Repository/AdvertisementRepository.java
@@ -1,10 +1,12 @@
 package se.lexicon.marketplaceapi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.marketplaceapi.domain.entity.Advertisement;
 
 import java.util.List;
 
+@Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, String> {
 
     List<Advertisement> findByTitleContains(String title);
--- src/main/java/se/lexicon/marketplaceapi/Repository/RoleRepository.java
+++ src/main/java/se/lexicon/marketplaceapi/Repository/RoleRepository.java
@@ -1,10 +1,12 @@
 package se.lexicon.marketplaceapi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.marketplaceapi.domain.entity.Role;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Optional<Role> findByName(String name);
