9c3ad95a666cacedbcbed95eb71bbfd1f5524b09
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-17 17:04:50 +0200

    Implemented

--- src/main/java/se/lexicon/marketplaceapi/Service/Impl/RoleServiceImpl.java
+++ src/main/java/se/lexicon/marketplaceapi/Service/Impl/RoleServiceImpl.java
@@ -1,4 +1,36 @@
 package se.lexicon.marketplaceapi.Service.Impl;
 
-public class RoleServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.lexicon.marketplaceapi.Converter.RoleConverter;
+import se.lexicon.marketplaceapi.Repository.RoleRepository;
+import se.lexicon.marketplaceapi.Service.RoleService;
+import se.lexicon.marketplaceapi.domain.dto.RoleDTOView;
+import se.lexicon.marketplaceapi.domain.entity.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final RoleConverter roleConverter;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository, RoleConverter roleConverter) {
+        this.roleRepository = roleRepository;
+        this.roleConverter = roleConverter;
+    }
+
+    @Override
+    public List<RoleDTOView> getAll() {
+        List<Role> roles = roleRepository.findAll();
+        List<RoleDTOView> roleDTOViewList = new ArrayList<>();
+        for (Role entity : roles) {
+            RoleDTOView dtoView = roleConverter.toRoleDTO(entity);
+            roleDTOViewList.add(dtoView);
+        }
+        return roleDTOViewList;
+    }
 }
