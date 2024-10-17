a1dcaec9198e9fd9af0f73821a0a8e783c466b7a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-17 15:35:04 +0200

    modified

--- src/main/java/se/lexicon/marketplaceapi/Converter/UserConverter.java
+++ src/main/java/se/lexicon/marketplaceapi/Converter/UserConverter.java
@@ -2,12 +2,13 @@ package se.lexicon.marketplaceapi.Converter;
 
 import org.springframework.stereotype.Component;
 import se.lexicon.marketplaceapi.domain.dto.UserDTOForm;
+import se.lexicon.marketplaceapi.domain.dto.UserDTOView;
 import se.lexicon.marketplaceapi.domain.entity.User;
 
 @Component
 public interface UserConverter {
 
-    UserConverter toUserDTO(User entity);
+    UserDTOView toUserDTO(User entity);
 
     User toEntity(UserDTOForm dto);
 }
