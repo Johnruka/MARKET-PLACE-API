e17477477f2274cc329300f370186fa8d7e0e000
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-19 19:01:56 +0200

    Added constructors and APIs

--- src/main/java/se/lexicon/marketplaceapi/controller/AdvertisementController.java
+++ src/main/java/se/lexicon/marketplaceapi/controller/AdvertisementController.java
@@ -1,4 +1,18 @@
 package se.lexicon.marketplaceapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.marketplaceapi.Service.AdvertisementService;
+
+@RequestMapping("/api/v1/advertisements")
+@RestController
 public class AdvertisementController {
+
+    private final AdvertisementService advertisementService;
+
+    @Autowired
+    public AdvertisementController(AdvertisementService advertisementService) {
+        this.advertisementService = advertisementService;
+    }
 }
