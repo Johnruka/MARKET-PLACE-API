cc14fe1955c2a1b3b8f24e76a9adc605b6e9cc07
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-19 18:58:43 +0200

    Added constructors and APIs

--- src/main/java/se/lexicon/marketplaceapi/controller/PersonController.java
+++ src/main/java/se/lexicon/marketplaceapi/controller/PersonController.java
@@ -1,4 +1,18 @@
 package se.lexicon.marketplaceapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.marketplaceapi.Service.PersonService;
+
+@RequestMapping("/api/v1/person")
+@RestController
 public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 }
