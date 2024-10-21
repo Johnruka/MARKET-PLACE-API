390348d1077b2832275be82863112668397421f1
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-21 09:17:04 +0200

    Added notifications

--- src/main/java/se/lexicon/marketplaceapi/config/SwaggerConfig.java
+++ src/main/java/se/lexicon/marketplaceapi/config/SwaggerConfig.java
@@ -1,4 +1,10 @@
 package se.lexicon.marketplaceapi.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Market place API", version = "0.1", description = "API Information"))
 public class SwaggerConfig {
 }
